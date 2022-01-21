d355f3a25ed6df7d82e6de20389129d7ed406d79
Karishma Daya <kdaya001@gmail.com>
2022-01-21 21:39:11 +1100

    fix upvote bug on profile

--- helpers/avatar.py
+++ helpers/avatar.py
@@ -4,5 +4,5 @@ def generate_random_number():
     return randint(0,100000)
 
 def generate_avatar():
-    response =  f'https://avatars.dicebear.com/api/bottts/:{generate_random_number()}.svg'
+    response =  f'https://avatars.dicebear.com/api/big-smile/:{generate_random_number()}.svg'
     return response
